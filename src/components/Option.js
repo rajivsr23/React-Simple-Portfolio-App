10387c9d898b3ce5e7febe4d3f5496af40ab3e41
Ricky Raghavan <rajivsr@yahoo.com>
2019-02-02 18:57:20 -0500

    Styled the Add Option Component

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
  const Option = (props) => (
-    <div>
+    <div className="option">
+    <p className="option__text">{props.count}. {props.optionText}</p>
       {props.optionText}
       <button
       className="button button--link"
