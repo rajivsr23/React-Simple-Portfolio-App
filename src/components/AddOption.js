d67d01043817036eca9d68128480b106b57cd20f
Ricky Raghavan <rajivsr@yahoo.com>
2019-02-02 15:01:24 -0500

    Initial Commit

---
+++ src/components/AddOption.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+
+export default class AddOption extends React.Component {
+  state={
+    error:undefined
+  };
+  constructor(props) {
+    super(props);
+    this.handleAddOption = this.handleAddOption.bind(this);
+  }
+  handleAddOption=(e) =>{
+    e.preventDefault();
+
+    const option = e.target.elements.option.value.trim();
+    const error = this.props.handleAddOption(option);
+
+    this.setState(() => ({ error }));
+  }
+  
+  render() {
+    return (
+      <div>
+        {this.state.error && <p>{this.state.error}</p>}
+        <form onSubmit={this.handleAddOption}>
+          <input type="text" name="option" />
+          <button>Add Option</button>
+        </form>
+      </div>
+    );
+  }
+}
