86c4b0c6c77964f4faa7519d5506bb87bf6f1aae
Ricky Raghavan <rajivsr@yahoo.com>
2019-02-02 17:49:52 -0500

    Applied Styling to the Header and the body via scss

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -63,7 +63,8 @@ export default class IndecisionApp extends React.Component {
     return (
       <div>
         <Header subtitle={subtitle} />
-        <Action
+        <div className="container">
+         <Action
           hasOptions={this.state.options.length > 0}
           handlePick={this.handlePick}
         />
@@ -75,6 +76,7 @@ export default class IndecisionApp extends React.Component {
         <AddOption
           handleAddOption={this.handleAddOption}
         />
+        </div>
         <OptionModal
         selectedOption={this.state.selectedOption}
         handleSelectedOption={this.handleSelectedOption}
