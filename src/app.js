8aa4b693bfd15dfeddcce0089a214bc777a8c6fd
Ricky Raghavan <rajivsr@yahoo.com>
2019-02-03 08:27:05 -0500

    Navigate between pages without full page refresh via Link/NavLink

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -31,13 +31,35 @@ This is from my Help Expense component
 </div>
 
 );
+
+const NotFoundPage =() =>(
+<div>
+404!- <Link to="/">Go Home!</Link>
+</div>
+
+);
+
+const Header=()=>(
+<header>
+<h1>Expensify</h1>
+<NavLink to="/" activeClassName="is-active" exact={true}>Home Page</NavLink><br/>
+ <NavLink to="/create" activeClassName="is-active">Create Expense Page</NavLink><br/>
+<NavLink to="/edit" activeClassName="is-active">Edit Page</NavLink><br/>
+<NavLink to="/help" activeClassName="is-active">Help Page</NavLink><br/>
+</header>
+
+);
 const routes =(
 <BrowserRouter>
 <div>
+<Header />
+<Switch>
 <Route path="/" component={ExpenseDashboardPage} exact={true}/>
 <Route path="/create" component={AddExpensePage}/>
 <Route path="/edit" component={EditExpensePage}/>
 <Route path="/help" component={HelpPage}/>
+<Route component={NotFoundPage}/>
+</Switch>
 </div>
 
 </BrowserRouter>
